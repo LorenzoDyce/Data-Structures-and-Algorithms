c0e510610d26cd14323b534685fd1ded3bf47ca4
Lorenzo <lpdmanagement@gmail.com>
2019-01-29 18:58:51 -0500

    Palindrome checker

---
+++ Palidrome.go
@@ -0,0 +1,9 @@
+package program
+
+func IsPalindrome(str string) bool {
+	result := []byte{}
+	for i := len(str) - 1; i >= 0; i-- {
+		result = append (result, str[i])
+	}
+	return str == string(string)
+}
